b9d482605dff585372cdf531e7bcc37b469d4583
jtheeeeee <j_03_17@naver.com>
2021-11-04 16:58:54 +0900

    item02

--- SpringLec/item02/src/main/java/com/sparta/item02/dto/MemoRequestDto.java
+++ SpringLec/item02/src/main/java/com/sparta/item02/dto/MemoRequestDto.java
@@ -1,2 +1,9 @@
-package com.sparta.item02.dto;public class MemoRequestDto {
+package com.sparta.item02.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemoRequestDto {
+    private String username;
+    private String contents;
 }
--- SpringLec/item02/src/main/java/com/sparta/item02/service/MemoService.java
+++ SpringLec/item02/src/main/java/com/sparta/item02/service/MemoService.java
@@ -1,2 +1,24 @@
-package com.sparta.item02.service;public class MemoService {
+package com.sparta.item02.service;
+
+import com.sparta.item02.domain.Memo;
+import com.sparta.item02.dto.MemoRequestDto;
+import com.sparta.item02.repository.MemoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class MemoService {
+
+    private final MemoRepository memoRepository;
+
+    @Transactional
+    public Long update(Long id, MemoRequestDto requestDto) {
+        Memo memo = memoRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
+        );
+        memo.update(requestDto);
+        return memo.getId();
+    }
 }
